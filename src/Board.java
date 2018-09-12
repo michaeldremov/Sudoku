aad6f26a8118f6fd7638921ec966a31cd1e37b50
Michael_Dremov <michael_dremov@whs1499.local>
2018-09-12 07:47:56 -0400

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,7 @@
+public class Board {
+
+//    private static String readLineByLine(String filepath) {
+////        StringBuilder
+//    }
+
+}
---
+++ src/SudokuMain.java
@@ -0,0 +1,8 @@
+public class SudokuMain {
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
