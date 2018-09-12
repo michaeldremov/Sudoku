ab281a1b696d9fe25bb0ea17a3eadbc35298370d
teamcaptainanthony <anthony789xue@gmail.com>
2018-09-12 08:09:45 -0400

    onnoe

--- src/Board.java
+++ src/Board.java
@@ -4,4 +4,20 @@ public class Board {
 ////        StringBuilder
 //    }
 
+
+    public String fillBoard(String textfile, int board){
+        for(int i = 0; i<textfile.length(); i++){
+
+
+
+            }
+
+            for(int j = 0; j<board.length(); j++){
+                for(int k = 0; k<board[0].length(); k++){
+
+                }
+            }
+        }
+    }
+
 }
